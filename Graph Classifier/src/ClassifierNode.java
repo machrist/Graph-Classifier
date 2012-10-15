aaa6091f0b8dbf8389666d7a0272c1352659ba94
machrist <mark-christopher@uiowa.edu>
2012-10-15 13:26:47 -0500

    First stuff...

---
+++ Graph Classifier/src/ClassifierNode.java
@@ -0,0 +1,142 @@
+import weka.classifiers.Classifier;
+import weka.classifiers.evaluation.EvaluationUtils;
+import weka.classifiers.evaluation.Prediction;
+import weka.core.FastVector;
+import weka.core.Instances;
+
+/**
+ * Creates a graph node wrapper around a weka Classifier.
+ * 
+ * @author mchristopher
+ */
+public class ClassifierNode {
+
+	/** Unique ID for this node */
+	String id;
+	
+	/** Classifier represented by this node */
+	Classifier classifier;
+	
+	/** Weight that should be associated with this node */
+	double weight;
+	
+	/**
+	 * Creates a node with the given id.
+	 * 
+	 * @param id
+	 *   Identifier to assign to this node. Should be unique.
+	 */
+	public ClassifierNode(String id){
+		this.id = id;
+	}
+	
+	/**
+	 * Builds the classifier model using the given training data.
+	 * 
+	 * @param data
+	 *   Training data used to build the classifier model
+	 * @return
+	 *   True if the classifierwas built successfully, false otherwise
+	 */
+	public boolean buildModel(Instances data){
+		boolean result = true;
+		try {
+			classifier.buildClassifier(data);
+		} catch (Exception e) {
+			e.printStackTrace();
+			result = false;
+		}
+		return result;
+	}
+	
+	/**
+	 * Evaluates the classifier using the given testing data.
+	 * 
+	 * This method sets the value of weight for this node based on the accuracy
+	 * on the testing data. The method getWeight() should be used to get the 
+	 * resulting weight value.
+	 * 
+	 * @param data
+	 *   Testing data used to evaluate the classifier
+	 * @return
+	 *   True if the classifier could be evaluated on this data, false otherwise
+	 */
+	public boolean evaluateOnData(Instances data){
+		
+		boolean result = true;
+		EvaluationUtils eval = new EvaluationUtils();
+		
+		try {
+			double right = 0, total = 0;
+			
+			FastVector<Prediction> results = (FastVector<Prediction>) eval.getTestPredictions(classifier, data);
+			
+			for(int i = 0; i < results.size(); ++i){
+				Prediction p = results.get(i);
+				if(p.predicted() == p.actual()){
+					right = right + 1;
+				}
+				total = total + 1;
+			}
+			
+			weight = right/total;
+			
+		} catch (Exception e) {
+			result = false;
+		}
+		
+		return result;
+	}
+	
+	/**
+	 * Gets the weight (proportional to the accuracy) that should be applied to 
+	 * this node. The accuracy used to compute the weight is determined during 
+	 * the evaluateOnData() method, so this method should only be called after 
+	 * that method.
+	 * 
+	 * @return 
+	 *   A weight value that should be associated with this node.
+	 */
+	public double getWeight(){
+		return weight;
+	}
+	
+	/**
+	 * Determines if this node is equal to another. Determination is based only
+	 * on node id.
+	 * 
+	 * @return
+	 *   True if o is a ClassifierNode and o.id == this.id, false otherwise
+	 */
+	public boolean equals(Object o){
+		if(o instanceof ClassifierNode){
+			return this.id.equals(((ClassifierNode) o).id);
+		}
+		return false;
+	}
+	
+	/**
+	 * Overridden to ensure equals/hashcode methods are consistent.
+	 */
+	public int hashCode(){
+		return id.hashCode();
+	}
+	
+	//Basic getters/setters
+	
+	public void setClassifier(Classifier c){
+		this.classifier = c;
+	}
+	
+	public Classifier getClassifier(){
+		return this.classifier;
+	}
+	
+	public void setID(String id){
+		this.id = id;
+	}
+	
+	public String getID(){
+		return this.id;
+	}
+}
--- Graph Classifier/src/GraphClassifier.java
+++ Graph Classifier/src/GraphClassifier.java
@@ -1,4 +1,101 @@
+import java.io.File;
+import java.util.Random;
 
-public class GraphClassifier {
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.SimpleGraph;
+
+import weka.classifiers.Classifier;
+import weka.core.Capabilities;
+import weka.core.Instance;
+import weka.core.Instances;
+import weka.core.converters.CSVLoader;
+import weka.filters.unsupervised.instance.Resample;
+
+
+public class GraphClassifier implements Classifier {
+	
+	/** Number of classifiers in the model */
+	int size;
+	
+	/** Fully qualified class name of the classifiers used to build ensemble model */
+	String classfierName;
+	
+	/** Arguments used to create classifiers. Passed to weka method Classifier.forName() */
+	String classArgs[];
+	
+	/** Training data set */
+	Instances trainData;
+	
+	public GraphClassifier(int n, String classifier, String args[]){
+		
+	}
+	
+	public void buildClassifier(Instances data){
+		this.trainData = data;
+		Resample sampler = null;
+		Random rand = new Random();
+		
+		SimpleGraph<ClassifierNode, DefaultEdge> model = new SimpleGraph<ClassifierNode, DefaultEdge>(DefaultEdge.class);
+		
+		ClassifierNode src = new ClassifierNode("s");
+		ClassifierNode sink = new ClassifierNode("t");
+		
+		model.addVertex(src);
+		model.addVertex(sink);
+		
+		for(int i = 0; i < this.size; ++i){
+			ClassifierNode c = new ClassifierNode(String.format("%02d", i));
+			model.addVertex(c);
+			model.addEdge(src, c);
+			model.addEdge(c, sink);
+		}
+		
+	}
+	
+	@Override
+	public double classifyInstance(Instance instance) throws Exception {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+	@Override
+	public double[] distributionForInstance(Instance instance) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public Capabilities getCapabilities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	
+	public void setClassifier(String c){
+		this.classfierName = c;
+	}
+	
+	public static void main(String args[]){
+		
+		int n = 100;
+		Instances data = null;
+		
+		CSVLoader csv = new CSVLoader();
+		try {
+			csv.setFile(new File(args[0]));
+			data = csv.getDataSet();
+			data.setClassIndex(data.numAttributes() - 1);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(-1);
+		}
+		
+		
+		GraphClassifier gc = new GraphClassifier(n, "weka.classifiers.trees.DecisionStump", null);
+		
+	}
 
 }
